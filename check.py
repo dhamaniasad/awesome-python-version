005c1891905a8cfda45a2b3c6410374850cbcdee
Asad Dhamani <dhamaniasad@gmail.com>
2014-10-21 17:27:35 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,20 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-driver = webdriver.PhantomJS()
+import urllib2
 f = open('out.txt', 'r')
-content = f.readlines()
-names = map(str.strip, content)
+names = map(str.strip, f)
 urls = ["https://pypi.python.org/pypi/" + str(x) for x in names]
 
-"""
-def getSource(name):
-    url = "https://pypi.python.org/pypi/%s" % (name)
-    # driver.get(url)
-    # WebDriverWait(driver, 3)
-    # page_source = driver.page_source
-    # return page_source
-    return url
 
-for name in names:
-    print getSource(name)
-"""
+def getsource(url):
+    open = urllib2.urlopen(url)
+    page_source = open.read()
+    return page_source
+
+
+for url in urls:
+    print getsource(url)
